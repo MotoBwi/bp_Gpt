22b26c6477230be030aa89a895a5bae1d38f8bd3
Bapan Sardar <130052982+MotoBwi@users.noreply.github.com>
2024-12-18 06:57:44 +0000

    intregreted APIs

--- jobot-web/src/pages/index.js
+++ jobot-web/src/pages/index.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
+import { useState } from "react";
+const SYSTEM_MESSAGE = "You are Jobot, a helpful and verstail AI created by Bapan Sardar using state-of arts ML Models and APIs.";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,7 +14,54 @@ const geistMono = Geist_Mono({
 });
 
 export default function Home() {
-  return (   
-    <div>Hello, World!</div>
-  );
+  const[apiKey, setapiKey] = useState('');
+  const[botMessage, setBotMessage] = useState("");
+
+const API_URL = "https://api.openai.com/v1/chat/completions";
+
+ async function sendRequest(){
+  const response = await fetch(API_URL,{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + apiKey,
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {"role": "system", content: SYSTEM_MESSAGE},
+        {"role": "user", content: "Hello!"}]
+    }),
+  });
+  const responseJson = await response.json();
+  
+  setBotMessage(responseJson.choices[0].message.content);
+
+  console.log("setBotMessage", setBotMessage);
+ }
+
+
+
+
+  return<div className="flex flex-col h-screen">
+    <nav className="shadow px-4 py-2 flex flex-row justify-between items-center">
+      <div className="text-xl font-bold">Jobot</div>
+      <div>
+        <input 
+        type="password" 
+        className="border p-2 rounded"
+        onChange={e => setapiKey(e.target.value)}
+        value={apiKey} 
+        placeholder="Paste API Key here"/>
+      </div>
+    </nav>
+    <div className="p-4">
+      <button 
+      className="border rounded-md p-2 bg-blue-500 hover:bg-blue-600 text-white"
+      onClick={sendRequest}>
+        Send Request
+        </button>
+        <div className="text-lg mt-4">{botMessage}</div>
+    </div>
+  </div>
 }
